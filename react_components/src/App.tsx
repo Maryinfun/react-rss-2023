c719b4ceae862a9a287d15132fc3cf853cfd9d81
Maryinfun <motmaryion@gmail.com>
2023-10-29 21:42:42 +0300

    init: task start

---
+++ react_components/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css'
+import StartPage from './components/StartPage';
+
+function App() {
+  return (
+    <StartPage />
+)
+}
+
+export default App
---
+++ react_components/src/components/StartPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function StartPage() {
+    return (
+        <div>
+            <h1>Dear reviewer, I would appreciate if you check my work at the end of cross-check. Thank you!</h1>
+        </div>
+    );
+}
+
+export default StartPage;
