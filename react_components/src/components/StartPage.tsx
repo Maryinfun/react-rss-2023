ed7e1e9884659caa846a2f64ec15b64d73070088
Maryinfun <motmaryion@gmail.com>
2023-11-01 13:19:38 +0300

    feat: correct

--- react_components/src/components/StartPage.tsx
+++ react_components/src/components/StartPage.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function StartPage() {
     return (
         <div>
-            <h1>Thank you for your understanding. Unfortunately I've done the task in part of setting up the environment only</h1>
+            <h1>Thank you for your understanding. Unfortunately I&#39;ve done the task in part of setting up the environment only</h1>
         </div>
     );
 }
